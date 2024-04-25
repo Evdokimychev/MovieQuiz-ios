01f110d3b63057a1db407241520b34ff7eadc28b
Ivan Evdokimychev <IAEvdokimychev@ya.ru>
2024-04-25 17:26:55 -0300

    Наладили взаимодействия между контроллером и фабрикой; устранили некоторые ошибки, которые возникли после переноса моковых данных

---
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,8 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Для учёбы   on 25/04/2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactory.swift
+//  MovieQuiz
+//
+//  Created by Для учёбы   on 25/04/2024.
+//
+
+import Foundation
