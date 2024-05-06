2d86df05e1891abd0c06a2019048b4859e6eed2c
Ivan Evdokimychev <IAEvdokimychev@ya.ru>
2024-05-06 17:12:25 -0300

    Реализовал паттерн делегат для фабрики

---
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryDelegate.swift
+//  MovieQuiz
+//
+//  Created by Для учёбы   on 26/04/2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Для учёбы   on 26/04/2024.
+//
+
+import Foundation
